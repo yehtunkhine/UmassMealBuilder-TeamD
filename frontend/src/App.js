d7a70a47e13f9549b32a526e977e11ce8b4e0c4d
OmerYSezer <omerysezer@gmail.com>
2023-03-25 16:19:39 -0400

    added react

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
