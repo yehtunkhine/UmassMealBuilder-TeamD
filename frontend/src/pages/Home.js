216abfd964bc90f53b27bd56f585372580a77b80
Ye Tun Khine (Kai) <yehtunkhine@gmail.com>
2023-04-05 19:23:12 -0400

    fix css issue

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
-import HeroSection from './components/HeroSection';
+import '../App.css';
+import HeroSection from '../components/HeroSection.css';
 
 function Home() {
   return (
