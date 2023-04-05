cfd5cdd9540e6de3565c43ce2ff48251c48eee94
lsuss18 <93808889+lsuss18@users.noreply.github.com>
2023-04-04 22:50:57 -0400

    subpages and routing
    Created subpages and routing for the dining halls

--- frontend/src/pages/DiningHalls.js
+++ frontend/src/pages/DiningHalls.js
@@ -1,19 +1,25 @@
+import { Link, useMatch, useResolvedPath} from "react-router-dom"
+
 const DINING_HALLS = [
     {
         "name": "Worcester Dining Hall",
         "imgUrl": "assets/worcester_dining_hall.jpg",
+        "link" : "/worcester",
     },
     {
         "name": "Franklin Dining Hall",
         "imgUrl": "assets/franklin_dining_hall.jpg",
+        "link" : "/franklin",
     },
     {
         "name": "Berkshire Dining Hall",
         "imgUrl": "assets/berkshire_dining_hall.jpg",
+        "link" : "/berkshire",
     },
     {
         "name": "Hampshire Dining Hall",
         "imgUrl": "assets/hampshire_dining_hall.png",
+        "link" : "/hampshire",
     },
 ]
 
@@ -23,7 +29,7 @@ export default function DiningHalls(){
             <div className="row">
                 {DINING_HALLS.map((dh) => (
                     <div className="col-12 col-md-6">
-                        <DiningCard imgUrl={dh.imgUrl} name={dh.name}/>
+                        <DiningCard imgUrl={dh.imgUrl} name={dh.name} link={dh.link}/>
                     </div>
                 ))}
             </div>
@@ -31,13 +37,29 @@ export default function DiningHalls(){
     )
 }
 
-const DiningCard = ({imgUrl, name}) => {
+const DiningCard = ({imgUrl, name,link}) => {
     return (
         <div className="dining-card">
             <img src={imgUrl} className=""/>
             <div className="card-content">
                 <h4>{name}</h4>
+                <CustomLink to = {link}>{name}</CustomLink>
             </div>
         </div>
     )
 }
+
+function CustomLink({ to,children, ...props}){
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({path: resolvedPath.pathname, end:true})
+       return(
+           <li className = {isActive ? "active":""}>
+               <Link to = {to}{...props}>
+                    {children}
+               </Link>
+           </li>
+       )
+   }
+   
+  
+  
