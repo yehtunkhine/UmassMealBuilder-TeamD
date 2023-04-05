0e71136b51ccbb42261503f52cfe3e8cd601dde1
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-04-05 03:33:27 -0400

    add open and close time for each dining hall

--- frontend/src/pages/DiningHalls.js
+++ frontend/src/pages/DiningHalls.js
@@ -4,21 +4,25 @@ const DINING_HALLS = [
     {
         "name": "Worcester Dining Hall",
         "imgUrl": "assets/worcester_dining_hall.jpg",
+        "time": "7:00 AM - 12:00 AM",
         "link" : "/worcester",
     },
     {
         "name": "Franklin Dining Hall",
         "imgUrl": "assets/franklin_dining_hall.jpg",
+        "time": "7:00 AM - 9:00 PM",
         "link" : "/franklin",
     },
     {
         "name": "Berkshire Dining Hall",
         "imgUrl": "assets/berkshire_dining_hall.jpg",
+        "time": "11:00 AM - 12:00 AM",
         "link" : "/berkshire",
     },
     {
         "name": "Hampshire Dining Hall",
         "imgUrl": "assets/hampshire_dining_hall.png",
+        "time": "7:00 AM - 9:00 PM",
         "link" : "/hampshire",
     },
 ]
@@ -29,7 +33,7 @@ export default function DiningHalls(){
             <div className="row">
                 {DINING_HALLS.map((dh) => (
                     <div className="col-12 col-md-6">
-                        <DiningCard imgUrl={dh.imgUrl} name={dh.name} link={dh.link}/>
+                        <DiningCard imgUrl={dh.imgUrl} name={dh.name} link={dh.link} time={dh.time}/>
                     </div>
                 ))}
             </div>
@@ -37,13 +41,14 @@ export default function DiningHalls(){
     )
 }
 
-const DiningCard = ({imgUrl, name,link}) => {
+const DiningCard = ({imgUrl, name,link, time}) => {
     return (
         <div className="dining-card">
             <Link to={link}>
                 <img src={imgUrl} alt=""/>
                 <div className="card-content">
                     <h4>{name}</h4>
+                    <p className="card-description">{time}</p>
                 </div>
             </Link>
         </div>
