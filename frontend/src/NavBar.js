85d565f32a5dc73e49fd5842fd30cddb05163a11
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-04-05 03:44:42 -0400

    navbar is now collapsible in medium-sized devices

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -2,16 +2,21 @@ import { Link, useMatch, useResolvedPath} from "react-router-dom"
 
 export default function Navbar(){
     return (
-        <nav className="navbar nav bg-maroon">
+        <nav className="navbar navbar-expand-md nav bg-maroon navbar-dark">
         <div className="container-fluid">
-        <Link to="/" className="navbar-brand site-title">UMass Meal Builder</Link>
-        <ul>
-            <CustomLink to = "/analysis">Analysis</CustomLink>
-            <CustomLink to = "/dininghalls">Dining Halls</CustomLink>
-            <CustomLink to = "/favorites">Favorites</CustomLink>
-            <CustomLink to = "/login">Login</CustomLink>
-            <CustomLink to = "/signup">Sign Up</CustomLink>
-        </ul>
+            <Link to="/" className="navbar-brand site-title">UMass Meal Builder</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul>
+                    <CustomLink to = "/analysis">Analysis</CustomLink>
+                    <CustomLink to = "/dininghalls">Dining Halls</CustomLink>
+                    <CustomLink to = "/favorites">Favorites</CustomLink>
+                    <CustomLink to = "/login">Login</CustomLink>
+                    <CustomLink to = "/signup">Sign Up</CustomLink>
+                </ul>
+            </div>
         </div>
     </nav>
     )
