4a7447f1f8b36feb496bcd07e914c48b731294fc
sivan-kandinov <105832736+sivan-kandinov@users.noreply.github.com>
2023-04-05 21:41:30 -0400

    homepage changes

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -6,22 +6,22 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <h1>heading 1</h1>
-      <p>paragraph</p>
+      <h1>Welcome to your Umass Dining meal builder</h1>
+      <p>description</p>
       <div className='hero-btns'>
         <Button
           className='btns'
           buttonStyle='btn--outline'
           buttonSize='btn--large'
         >
-          button 1
+          See how it works
         </Button>
         <Button
           className='btns'
           buttonStyle='btn--primary'
           buttonSize='btn--large'
         >
-          button 2
+          Try Now
         </Button>
       </div>
     </div>
