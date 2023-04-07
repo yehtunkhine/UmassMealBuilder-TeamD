6eb332df9bced70584512e4f0b0c1adbd1a8c23f
Jhunt2020 <jahkobeehunt@umass.edu>
2023-04-07 15:16:27 -0400

    Accordion
    Accordion made, food cards definitely a work in progress

---
+++ frontend/src/components/AccData.js
@@ -0,0 +1,34 @@
+export const Data = [
+    {
+        category: 'Breakfast',
+    },
+    {
+        category: "Lunch",
+    },
+    {
+        category: "Dinner",
+    },
+    {
+        category: "Late Night",
+    }
+];
+
+
+export const FoodData = [
+    {
+        name: "Food1",
+        image: "assets/food1.png"
+    },
+    {
+        name: "Food2",
+        image: "assets/food2.png"
+    },
+    {
+        name: "Food3",
+        image: "assets/food3.png"
+    },
+    {
+        name: "Food4",
+        image: "assets/food4.png"
+    }
+];
