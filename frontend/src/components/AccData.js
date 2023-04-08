24c5be6d68229005a249063890becce2b64f005b
Jhunt2020 <jahkobeehunt@umass.edu>
2023-04-08 18:11:33 -0400

    Displays item information on new page
    Clicking on items with available data will display that data on a separate page

--- frontend/src/components/AccData.js
+++ frontend/src/components/AccData.js
@@ -13,41 +13,58 @@ export const Data = [
     }
 ];
 
+export const MealItems = {
+    "Chicken Noodle Soup" : {
+        name: "Chicken Noodle Soup",
+        nutrientInfo: {
+            calories: "223",
+            fat: "4.9g",
+            saturatedFat: "2.3g",
+            protein: "5.9g",
+            carbohydrates: "39.4g"
+        },
+        ingredients: "Chicken, Noodles, Water, Salt, Carrots, Pepper",
+        allergens: "None",
+        recipeLables: "Recipe Label Stuff",
+        healthfulness: "30",
+        servingSize: "3 Scoops"
+    },
+    "Creamy Tomato Soup" : {
+        name: "Creamy Tomato Soup",
+        nutrientInfo: {
+            calories: "300",
+            fat: "4.9g",
+            saturatedFat: "2.3g",
+            protein: "5.9g",
+            carbohydrates: "39.4g"
+        },
+        ingredients: "Tomato, Milk, Salt, Pepper, Water, Oil",
+        allergens: "Milk",
+        recipeLables: "Recipe Label Stuff",
+        healthfulness: "40",
+        servingSize: "2 Scoops"
+    } 
 
-export const FoodData = [
-    {
-        name: "Food1",
-        image: "assets/food1.png"
+
+
+}
+/*
+"Baked French Toast w/Banana Maple Syrup and Ricotta": {
+    "name": "Baked French Toast w/Banana Maple Syrup and Ricotta",
+    "nutrientInfo": {
+        "calories": "223",
+        "fat": "4.9g",
+        "saturatedFat": "2.3g",
+        "protein": "5.9g",
+        "carbohydrates": "39.4g"
     },
-    {
-        name: "Food2",
-        image: "assets/food2.png"
-    },
-    {
-        name: "Food3",
-        image: "assets/food3.png"
-    },
-    {
-        name: "Food4",
-        image: "assets/food4.png"
-    },
-    {
-        name: "Food5",
-        image: "assets/food1.png"
-    },
-    {
-        name: "Food6",
-        image: "assets/food2.png"
-    },
-    {
-        name: "Food7",
-        image: "assets/food3.png"
-    },
-    {
-        name: "Food8",
-        image: "assets/food4.png"
-    }
-];
+    "ingredients": "Fresh Bananas, White Texas Toast (Enriched Wheat Flour[(Flour, Malted Barley Flour, Niacin, Reduced Iron, Thiamin Mononitrate, Riboflavin, Folic Acid], Water, High Fructose Corn Syrup, Contains 2% of Less of: Yeast, Wheat Gluten, Soybean Oil, Salt, Sodium Stearoyl Lactylate, Monoglycerides, Calcium Sulfate, Calcium Peroxide, Soy Lecithin, Calcium Propionate), Mapleline Whole Milk, Local Pure Maple Syrup, Cage Free Eggs, Ricotta Cheese (Pasteurized Whole Milk, Vinegar, Salt, Locust Bean Gum, Xanthan Gum, Guar Gum), Local Unsalted Butter (Cream (Milk), Natural Flavoring), Golden Brown Sugar (Brown Sugar), EAGLE BRAND Sweetened Condensed Milk (Milk, Sugar), Imitation Vanilla Flavouring (Water, Natural and Artificial Flavors, Propylene Glycol, Caramel Color (May Contain Corn)), Cinnamon, Kosher Salt (Salt. MORTON: Salt, Yellow Prussiate of Soda)",
+    "allergens": "Milk, Eggs, Gluten, Soy, Corn  , Wheat",
+    "recipeLabels": "SUS VGT H3 CR1",
+    "healthfulness": "30",
+    "servingSize": "1 EACH"
+},
+*/
 
 export const MealData = [
     {
