d17d74b7990e52a859de3d057b1b9e8eef28489c
Ap0phi15 <chollenbeck@umass.edu>
2023-04-16 17:49:01 -0400

    added to user.js
    added some calls to create and get data. not done yet, but i think I did it right based on the template

--- backend/src/rest-api/users.js
+++ backend/src/rest-api/users.js
@@ -12,11 +12,80 @@ async function testConnection() {
 }
  
 testConnection()
+//create user
+async function createUser(username, useremail, userphone){
+  const newUser=await User.create({userID: Date.now()+"", name: username, email: useremail, phone: userphone});
+  return JSON.stringify(newUser);
+}
+
+app.get('/createUser', (req, res)=>{
+  (async function createAndSend(){
+    let sendVal = await createUser(req.name, req.email, req.phone)
+    res.end(sendVal)
+  })();
+
+})
+
+//retrieve user data
+async function fetchUserData(username){
+  let f_user =[];
+  try{
+    const users = await User.findAll({
+      where: {
+        name: username,
+      }
+    });
+    users.forEach(user =>{
+      f_user.push({
+        id: user.userID,
+        name: user.name,
+        email: user.eamil,
+        phone: user.phone
+      })
+    });
+  } catch(err){
+    console.log("Username does not exist");
+  }
+  return f_user;
+}
+
+app.get('/getUser', (req, res) =>{
+  (async function getUser(){
+    let users = fetchUserData(req.name)
+    let str = JSON.stringify(users)
+    res.end(str)
+  })();
+})
+//fetch user restrictions
+async function fetchUserRestrictions(userid){
+  let user_rest= []
+  try{
+    const user_data= await UserRestriction.findAll({
+      where:{
+        userID: userid,
+      }
+    });
+    user_rest.push(user_data.restriction)
+  } catch(err){
+    console.log("userId does not exist or has no restrictions")
+  }
+  return user_rest;
+}
+
+app.get('/getUserRestrictions', (req, res)=>{
+  (async function getUserRestrictions(){
+    let restrict = await fetchUserRestrictions(req.userid)
+    res.end(restrict)
+  })();
+})
 
 
-//retrieve user PK
-console.log(seq)
 
 
 
-//create new user PK
+
+
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`)
+});
