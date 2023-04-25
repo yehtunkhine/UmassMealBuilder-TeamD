2f5b84f3191300920e4479799630178da2cee460
OmerYSezer <omerysezer@gmail.com>
2023-04-25 14:01:05 -0400

    added default favorite location - fixed

--- backend/src/rest-api/models.js
+++ backend/src/rest-api/models.js
@@ -275,27 +275,26 @@ LocationTimes.removeAttribute('id')
 // Push to db
 // Change models above and then uncomment and run this file to make db changes
 // force option will wipe database before updating tables!!!
-await sequelize.sync({force: true})
+// await sequelize.sync({force: true})
 
 
-await User.create({userId: "123456789", email: "random@email.com", phone: "555-555-5555", name: "John Doe"});
-await Location.create({locationName: "Worcester"});
-let oreo_ingredient = "Unbleached Enriched Flour (Wheat Flour Niacin, Reduced Iron, Thiamine Mononitrate {Vitamin B1}," + 
-    "Riboflavin {Vitamin B2}, Folic Acid), Sugar, Palm and/or Canola Oil, Cocoa (Processed with Alkali), High Fructose" +
-    "Corn Syrup, Leavening (Baking Soda, and/or Calcium Phosphate), Salt, Soy Lecithin, Chocolate, Artificial Flavor.";
-await Food.create({name: "Oreos", category: "Candy", calories: 140, fat: 7, protein: 0, carbs: 21, saturated_fat: 2, 
-            ingredients: oreo_ingredient, servingSize: "2 Cookies"});
-await LocationFoodBridge.create({locationId: 1, foodId: 1, Time: "Breakfast", Date: "2022-06-23"});
-await FavoriteLocationsBridge.create({userId: "123456789", locationId: 1});
+// await User.create({userId: "123456789", email: "random@email.com", phone: "555-555-5555", name: "John Doe"});
+// await Location.create({locationName: "Worcester"});
+// let oreo_ingredient = "Unbleached Enriched Flour (Wheat Flour Niacin, Reduced Iron, Thiamine Mononitrate {Vitamin B1}," + 
+//     "Riboflavin {Vitamin B2}, Folic Acid), Sugar, Palm and/or Canola Oil, Cocoa (Processed with Alkali), High Fructose" +
+//     "Corn Syrup, Leavening (Baking Soda, and/or Calcium Phosphate), Salt, Soy Lecithin, Chocolate, Artificial Flavor.";
+// await Food.create({name: "Oreos", category: "Candy", calories: 140, fat: 7, protein: 0, carbs: 21, saturated_fat: 2, 
+//             ingredients: oreo_ingredient, servingSize: "2 Cookies"});
+// await LocationFoodBridge.create({locationId: 1, foodId: 1, Time: "Breakfast", Date: "2022-06-23"});
+// await FavoriteLocationsBridge.create({userId: "123456789", locationId: 1});
 
-let user = await User.findOne({
-    where: {userId: '123456789'}
-});
-let food = await Food.findOne({
-    where: {foodId: 1}
-});
-user.addFood(food);
-
+// let user = await User.findOne({
+//     where: {userId: '123456789'}
+// });
+// let food = await Food.findOne({
+//     where: {foodId: 1}
+// });
+// user.addFood(food);
 
 // Export Models For Other Files
 export {User, Food, FoodRestriction, UserRestriction, Meal, Location, LocationTimes, LocationFoodBridge, FavoriteLocationsBridge};
