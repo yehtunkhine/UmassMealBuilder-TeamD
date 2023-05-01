429f860881a857bbf337380ae5751da47b843d30
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-05-01 16:40:36 -0400

    Fix attribute error

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <footer>
-            <p>Made with <span><i class="fa fa-heart"></i></span> by Team D</p>
+            <p>Made with <span><i className="fa fa-heart"></i></span> by Team D</p>
         </footer>
     );
 }
