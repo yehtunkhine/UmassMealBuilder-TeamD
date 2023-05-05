4f8f75b0da1ff818cff26d99d2738030be8b6370
Joseph Collins <jcollins2021@gmail.com>
2023-05-05 18:17:09 -0400

    Allows to add and remove restrictions
    Also added TODOs for integration, should be simple

--- frontend/src/pages/User.js
+++ frontend/src/pages/User.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useContext } from 'react';
 import { AuthenticationContext } from './../App';
 import { useRef } from 'react';
@@ -7,6 +7,8 @@ import { sendPasswordResetEmail } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 //import { EmailAuthCredential, updatePassword } from 'firebase/auth';
 
+let firstRun = true;
+let trackState = [];
 
 export default function User(){
     let auth = useContext(AuthenticationContext);
@@ -15,6 +17,8 @@ export default function User(){
     const settingsRef = useRef(null);
     const favoritesRef = useRef(null);
     const navigate = useNavigate();
+    const [userRestrictionsState, setUserRestrictionsState] = useState(trackState);
+
     /* In case we can ever fix the user authentication
     const txtPassword = useRef(null);
     const txtNewPassword = useRef(null);
@@ -122,12 +126,81 @@ export default function User(){
         }
     }  
 
+    const getUserRestrictions = () =>{
+        let restrictions = document.getElementById("userRestrictions");
+        let removal = document.getElementById("userRestrictionsRemoval");
+        while(restrictions.firstChild){
+            restrictions.removeChild(restrictions.firstChild);
+        }
+        while(removal.firstChild){
+            removal.removeChild(removal.firstChild);
+        }
+        let userRestrictionsArray = userRestrictionsState;
+        for(let i = 0; i < userRestrictionsArray.length; i++){
+            var el = document.createElement("li");
+            el.textContent = userRestrictionsArray[i];
+            var remove = document.createElement("button");
+            remove.textContent = "\xD7";
+            remove.className = "removal-button";
+            remove.onclick = function(){
+                removeUserRestriction(userRestrictionsArray[i]);
+            };
+            removal.appendChild(remove);
+            restrictions.appendChild(el);
+        }
+    }
+
+    const removeUserRestriction = (child) => {
+        let tempState = [...trackState];
+        tempState.splice(tempState.indexOf(child), 1);
+        trackState = tempState;
+        //TODO set userdata to trackState
+        setUserRestrictionsState(trackState); 
+    }
+
+    const setUserRestrictions = () => {
+        //TODO set userRestrictionsArray to user data
+        //Integrate here, this will only trigger on initial run through
+        let userRestrictionsArray = [];
+        userRestrictionsArray.push("Sample Restriction");
+        userRestrictionsArray.push("Sample Restriction2");
+        userRestrictionsArray.push("Sample");
+        trackState = userRestrictionsArray;
+        setUserRestrictionsState(userRestrictionsArray);
+    }
+
+    const addUserRestrictions = () =>{
+        let userRestrictionsArray = [...userRestrictionsState];
+        let tempRestriction = document.getElementById("allergen").value;
+        if(!userRestrictionsArray.includes(tempRestriction)){
+            userRestrictionsArray.push(tempRestriction);
+            trackState = userRestrictionsArray;
+            //Sort if have time
+            //Then push array to database
+            //TODO push new restriction to user data
+            setUserRestrictionsState(userRestrictionsArray);
+        }
+    }
+
+
     useEffect(() => {
-        if(user === null){
+        if(firstRun){
+            firstRun = false;
+            setUserRestrictions();
+        }
+        if(user !== null){
+            getRestrictions();
+            getUserRestrictions();
+            
+        }else{
             navigate("/");
         }
-        getRestrictions();
+        console.log(userRestrictionsState);
     });
+    if(user === null){
+        return <div>   
+            </div>;
+    }
     return (  
     <div class ="flex-container">
         <div class="sidenav flex-child-nav">
@@ -144,24 +217,39 @@ export default function User(){
             </div>
             <div class = "userBox">
                 <h4>Add a restriction</h4>
-                <select id="allergen"></select>
-                <button >Add Restriction</button>
+                <div class = "flex-container">
+                    <div class = "flex-space"></div>
+                    <select id="allergen" class = "flex-select "></select>
+                    <div class="flex-space"></div>
+                    <button onClick={addUserRestrictions} class = "flex-button  restrictions-button">Add Restriction</button>
+                    <div class = "flex-space"></div>
+                </div>
+                <br></br><br></br>
             <div class="separate"></div>
                 <h4>Current Restrictions</h4>
+                <div class = "flex-container">
+                <ul id="userRestrictions" class = "flex-list"></ul>
+                <ul id="userRestrictionsRemoval" class = "flex-remove"></ul>
+                </div>
             </div>
             <br></br>
-            <div class = "userBox" ref = {favoritesRef}>
+            <div class = "userBox labelBox" ref = {favoritesRef}>
                 <h3>Upcoming Favorites</h3>
             </div>
+            <div class= "userBox">
+            </div>
             <br></br>
-            <div class = "userBox" ref = {settingsRef}>
+            <div class = "userBox labelBox" ref = {settingsRef}>
                 <h3>Settings</h3>
+            </div>
+            <div class = "userBox">
                 <div class = "innerBox">
                     <button onClick = {handleDropdown} class="dropdown-btn">Change Password 
                         <i class="fa fa-caret-down"></i>
                     </button>
                     
                     <div class="dropdown-container">
+                        <div class = "separate"></div>
                         <button class = "" onClick={sendPasswordReset} >Send Password Reset Link</button>
                     </div>
                 </div>
